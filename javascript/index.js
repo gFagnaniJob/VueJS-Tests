b0a803ece6759d272c55394f7d8100cee05d1354
gFagnaniJob <gfagnani.job@gmail.com>
2018-09-24 15:14:06 +0200

    index completato

---
+++ javascript/index.js
@@ -0,0 +1,67 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+var app2 = new Vue({
+    el: "#app2",
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+
+var app3 = new Vue({
+    el: "#app3",
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: "#app4",
+    data: {
+        todos: [
+            {text: 'Learn JavaScript'},
+            {text: 'Lean Vue'},
+            {text: 'Build something awesome'}
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: "#app5",
+    data: {
+        message: "Ciao Mondo Infame"
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('');
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: "#app6",
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: "#app7",
+    data: {
+        groceryList: [
+            {id: 0, text: 'Vegetables'},
+            {id: 1, text: 'Cheese'},
+            {id: 2, text: 'Whatever else humans are supposed to eat'}
+        ]
+    }
+})
